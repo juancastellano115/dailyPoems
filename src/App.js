a24cb117451e778144ed6d8a08c01bf9aa1d1063
juancastellano115 <juancastellano115@gmail.com>
2020-08-18 21:05:38 +0200

    1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import Loader from "./components/loader";
+import useAxios from "axios-hooks";
 function App() {
+  const [{ data, loading, error }] = useAxios("https://poetrydb.org/random");
+  if (loading) return <Loader />;
+  if (error) return <p>Error!</p>;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{data.title}</h1>
+        <div className="textHolder">
+          <p>{data.lines}</p>
+        </div>
       </header>
     </div>
   );
